64e0d183f589dabb9327e3f17b8b9fff4bad25b4
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2025-03-17 15:45:44 +0530

    Added New Files

--- Concepts/printworld.py
+++ Concepts/printworld.py
@@ -1,5 +1,14 @@
 print("hello world")
-print("hello worl")
+print("hello world")
 print("Peddireddy Hari Vardhan Reddy")
 print("Peddireddy hari ")
-print
+print("Peddireddy")
+
+def main():
+    print("calling main method ..... ")
+
+
+main()
+
+for i in range(10):
+    print(i,end=" ")
