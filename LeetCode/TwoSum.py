f69d1d32b4c4e8c3a0620fcc1500f8f60de1f6ff
Hari.Balaji4674 <hari.balaji4674@gmail.com>
2025-04-14 12:53:02 +0900

    14-04-2025

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -1,7 +1,10 @@
 def twosum(nums : list[int],target: int) -> list[int]:
+    dict = {}
     for i in range(len(nums)):
-        print(nums[i])
+        
+        print(i,nums[i])
 
 list1 = [2,3,4,5,6,7]
 twosum(list1,8)
+print(dict())
 # print()
