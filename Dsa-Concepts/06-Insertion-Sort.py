8609631c388f7573a65cd0509ced5c18c7f0158f
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-04-26 19:26:57 +0530

    26-04-2025

--- Dsa-Concepts/06-Insertion-Sort.py
+++ Dsa-Concepts/06-Insertion-Sort.py
@@ -2,9 +2,9 @@ def insertionSort(list):
     for i in range(1,len(list)):
         temp = list[i]
         j = i - 1
-        while temp < list[j] and j > -1:
-            list[j+1] = list[j]
-            list[j] = temp
+        while temp < list[j] and j > -1: # 6 < 6 and j = 0 4 < 6 
+            list[j+1] = list[j] # list[1] = 6 , list[2] = 6
+            list[j] = temp # list[0] = 6
             j = j - 1
     return list
 
